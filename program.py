f891c77f26e086842ac5c99c8aeb7d58e3bcaecd
kenjiaydinw-sketch <kenjiaydinw@gmail.com>
2026-09-21 08:16:51 +0700

    Kerangka awal program bersama

---
+++ program.py
@@ -0,0 +1,10 @@
+def input_data():
+    pass  # akan diisi oleh Akun-1
+
+def buat_laporan(data):
+    pass  # akan diisi oleh Akun-2
+
+if __name__ == "__main__":
+    data = input_data()
+    buat_laporan(data)
+    
